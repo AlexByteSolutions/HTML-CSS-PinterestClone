7cba8196c51c4d14ca88de1cdfb76973529dfe03
AlexByteSolutions <159472942+AlexByteSolutions@users.noreply.github.com>
2025-08-21 19:41:11 -0300

    correção de bug da calculadora #2 + adição de pasta .github/workflows

--- calculadora/script.js
+++ calculadora/script.js
@@ -29,3 +29,14 @@ function calculate(operation) {
 
     document.getElementById('resultado').textContent = result;
 }
+
+document.querySelectorAll('a.nav-link').forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
